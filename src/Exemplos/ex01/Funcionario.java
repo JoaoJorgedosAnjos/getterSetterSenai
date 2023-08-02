88ab5d044a8f066bf5b370fb78b31a7cfba0b90b
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-01 22:55:50 -0300

    exercicio 01

--- src/Exemplos/ex01/Funcionario.java
+++ src/Exemplos/ex01/Funcionario.java
@@ -1,2 +1,43 @@
-package Exemplos.ex01;public class Funcionario {
+package Exemplos.ex01;
+
+public class Funcionario {
+    private String nome;
+    private String setor;
+    private double salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+    public double calcularImposto(double salario){
+        if (salario >= 1903.99 && salario <= 2826.65) {
+            return salario - (salario * 0.075);
+        } else if (salario >= 2826.66 && salario <= 3751.05) {
+            return salario - (salario * 0.15);
+        }else if (salario >= 3751.06 && salario <= 4664.68) {
+            return salario - (salario * 0.225);
+        }else if (salario > 4664.68) {
+            return salario - (salario * 0.275);
+        }
+        return salario;
+    }
 }
--- src/Exemplos/ex01/Main.java
+++ src/Exemplos/ex01/Main.java
@@ -1,2 +1,16 @@
-package Exemplos.ex01;public class Main {
+package Exemplos.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario f1 = new Funcionario();
+
+        f1.setNome("Marcos");
+        f1.setSetor("Vendedor");
+        f1.setSalario(4664.69);
+
+        System.out.println(f1.getNome() + "\n" + f1.getSetor() + "\n" + f1.getSalario());
+        System.out.printf("Nome do funcionário: %s\nSetor: %s\nSalário: %.2f\n", f1.getNome(), f1.getSetor(), f1.getSalario());
+
+        System.out.printf("Valor do salário com desconto: %.2f", + f1.calcularImposto(f1.getSalario()));
+    }
 }
